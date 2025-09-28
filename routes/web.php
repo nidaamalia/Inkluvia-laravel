9a579a659908080df45d32571d46b5a7c8a61af2
nidaamalia <nidacil@gmail.com>
2025-09-28 16:37:25 +0700

    Fix library page

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\PerpustakaanController;
 use App\Http\Controllers\MaterialController;
 use App\Http\Controllers\MaterialRequestController;
 use App\Http\Controllers\JadwalController;
+use App\Http\Controllers\PerpustakaanController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -66,9 +67,7 @@ Route::middleware('auth')->group(function () {
         Route::get('/perpustakaan/{material}/preview-page', [PerpustakaanController::class, 'showPreview'])->name('perpustakaan.preview-page');
         Route::get('/perpustakaan/{material}/send', [PerpustakaanController::class, 'sendToDevice'])->name('perpustakaan.send');
         Route::get('/request-materi', [\App\Http\Controllers\MaterialRequestController::class, 'userIndex'])->name('request-materi');
-        Route::get('/perpustakaan', function () {
-            return view('user.perpustakaan');
-        })->name('perpustakaan');
+        Route::get('/perpustakaan', [PerpustakaanController::class, 'index'])->name('perpustakaan');
     });
     
     // Admin routes
