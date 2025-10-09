3e61ab54f83171001c217287764a957f37140f10
nidaamalia <nidacil@gmail.com>
2025-10-09 08:32:40 +0700

    Add missing component registration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\View\Components\Badge;
+use App\View\Components\Detail;
+use App\View\Components\StatusRow;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::component('detail', Detail::class);
+        Blade::component('status-row', StatusRow::class);
+        Blade::component('badge', Badge::class);
     }
 }
